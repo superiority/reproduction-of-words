9be15f573fd108b4e6b1b4bfc11c54f0f8b0e7f2
777 <agadjiev397@gmail.com>
2023-01-01 16:21:17 +0300

    alert

--- script.js
+++ script.js
@@ -5,14 +5,20 @@ const speakEl = document.getElementById('speak');
 // перехват клик по кнопке
 speakEl.addEventListener('click', speakText);
 
+
+
 function speakText() {
     window.speechSynthesis.cancel();
+
+    if (textEl.value === '') {
+        alert('none')
+        return;
+    }
 
     // прочитать текст
     const text = textEl.value;
     const utterance = new SpeechSynthesisUtterance(text);
     window.speechSynthesis.speak(utterance);
-
 }
 
 
